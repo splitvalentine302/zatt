edee8ef2bf6c47e992d7047d8a882978db27e743
Maksym Bilan <maximb.mail@gmail.com>
2026-03-20 09:22:10 +0100

    release: v1.0.5

--- Formula/zatt.rb
+++ Formula/zatt.rb
@@ -1,9 +1,9 @@
 class Zatt < Formula
   desc "macOS CLI to control MacBook battery charging via SMC"
   homepage "https://github.com/maximbilan/zatt"
-  url "https://github.com/maximbilan/zatt/releases/download/v0.1.4/zatt-macos-arm64.tar.gz"
-  sha256 "16f09756795bb91c175abf9bd6dd4455ca482e6a0dac9d4dcc956a7fc27ecc5d"
-  version "0.1.4"
+  url "https://github.com/maximbilan/zatt/releases/download/v1.0.5/zatt-macos-arm64.tar.gz"
+  sha256 "7caa1cd062fa10d958f4eeae9f9c5121fe73d3f22dbb74fb41fd8c0854b00dc7"
+  version "1.0.5"
   license "MIT"
 
   depends_on arch: :arm64
